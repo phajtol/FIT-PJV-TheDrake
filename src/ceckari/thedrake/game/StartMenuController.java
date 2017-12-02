0819a019efa43517b4ef524fe8f602c87cf8f615
Peter Hajtol <hajtopet@fit.cvut.cz>
2017-12-02 16:50:59 +0100

    Added Start Screen easter egg - flags on logo change when label 'ceckari' is clicked.

--- src/ceckari/thedrake/game/StartMenuController.java
+++ src/ceckari/thedrake/game/StartMenuController.java
@@ -1,4 +1,3 @@
-
 package ceckari.thedrake.game;
 
 import java.awt.event.ActionEvent;
@@ -9,22 +8,44 @@ import javafx.event.EventHandler;
 import javafx.fxml.Initializable;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 
 /**
  *
  * @author friedtad
  */
-public class StartMenuController implements Initializable{
+public class StartMenuController implements Initializable {
+
     @FXML private Button endButton;
-    @FXML private Pane pane;
+    @FXML private AnchorPane root;
+    @FXML private Button networkGameButton;
+    @FXML private Button multiplayerButton;
+    @FXML private Button singleplayerButton;
+    @FXML private Label ceckariLabel;
+    @FXML private ImageView drakelogo;
+    
+    //true if currently shown logo is inverted (flags on opposite sides as usually)
+    private boolean invertedLogo = false;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        endButton.setOnAction(new EventHandler(){
+        endButton.setOnAction(new EventHandler() {
             @Override
             public void handle(Event event) {
                 System.exit(0);
+            }
+        });
+        
+        ceckariLabel.setOnMouseReleased(new EventHandler(){
+            @Override
+            public void handle(Event event) {
+                drakelogo.setImage(new Image(getClass().getResource(invertedLogo?"assets/drake-logo.png":"assets/drake-logo_inv.png").toExternalForm()));
+                invertedLogo = !invertedLogo;
             }
         });
     }
